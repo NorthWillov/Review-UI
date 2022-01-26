67eddf05e7583cfaec218636e7b6cbe5959ae7b3
NorthWillov <northwillov@gmail.com>
2022-01-26 10:24:24 +0100

    first commit

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,20 @@
+<script>
+  // import { onMount, onDestroy } from "svelte"
+  import { FeedbackStore } from "../stores"
+  import FeedbackItem from "./FeedbackItem.svelte"
+  import { fade, scale } from "svelte/transition"
+
+  // let feedback = []
+
+  // const unsubscribe = FeedbackStore.subscribe((data) => (feedback = data))
+
+  // onDestroy(() => {
+  //   unsubscribe()
+  // })
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+  <div in:scale out:fade={{ duration: 500 }}>
+    <FeedbackItem item={fb} />
+  </div>
+{/each}
---
+++ src/stores.js
@@ -0,0 +1,19 @@
+import { writable } from "svelte/store"
+
+export const FeedbackStore = writable([
+  {
+    id: 1,
+    rating: 10,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, pariatur. Nesciunt libero sequi illum distinctio autem! Unde saepe quisquam quod soluta fuga quaerat, facere tempora laborum, perferendis accusamus totam labore nostrum placeat iusto ea omnis? Asperiores ut ullam tempore, nam inventore enim est provident, minima illum vitae distinctio placeat nemo accusantium ratione velit commodi ab officiis rerum eum repudiandae! Dolores iure nisi debitis earum, corrupti dignissimos quidem eaque eveniet non tenetur provident ipsum ea. Iusto cupiditate, error suscipit aspernatur alias commodi qui doloremque id perspiciatis deleniti consectetur enim culpa tempora.",
+  },
+  {
+    id: 2,
+    rating: 9,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, pariatur. Nesciunt libero sequi illum distinctio autem! Unde saepe quisquam quod soluta fuga quaerat, facere tempora laborum, perferendis accusamus totam labore nostrum placeat iusto ea omnis? Asperiores ut ullam tempore, nam inventore enim est provident, minima illum vitae distinctio placeat nemo accusantium ratione velit commodi ab officiis rerum eum repudiandae! Dolores iure nisi debitis earum, corrupti dignissimos quidem eaque eveniet non tenetur provident ipsum ea. Iusto cupiditate, error suscipit aspernatur alias commodi qui doloremque id perspiciatis deleniti consectetur enim culpa tempora.",
+  },
+  {
+    id: 3,
+    rating: 8,
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Necessitatibus, pariatur. Nesciunt libero sequi illum distinctio autem! Unde saepe quisquam quod soluta fuga quaerat, facere tempora laborum, perferendis accusamus totam labore nostrum placeat iusto ea omnis? Asperiores ut ullam tempore, nam inventore enim est provident, minima illum vitae distinctio placeat nemo accusantium ratione velit commodi ab officiis rerum eum repudiandae! Dolores iure nisi debitis earum, corrupti dignissimos quidem eaque eveniet non tenetur provident ipsum ea. Iusto cupiditate, error suscipit aspernatur alias commodi qui doloremque id perspiciatis deleniti consectetur enim culpa tempora.",
+  },
+])
